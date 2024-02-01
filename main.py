646137a26dc0a27a38f1103cf26fe62059289c21
Hakan Yorganci <hakanyorganci@gmail.com>
2024-02-01 15:19:37 -0600

    Update main.py
    Main login screen phase 1

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
+
+class LoginScreen(Screen):
+    def check_credentials(self):
+        if self.ids.username.text == 'Hakan' and self.ids.password.text == '00000':
+            self.manager.current = 'main_screen'
+
+class MainScreen(Screen):
+    pass
+
+class CoolPoolApp(App):
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(LoginScreen(name='login_screen'))
+        sm.add_widget(MainScreen(name='main_screen'))
+
+        Window.clearcolor = (0.68, 0.78, 0.81, 1)  # Pastel blue background color
+        return sm
+
+if __name__ == '__main__':
+    CoolPoolApp().run()
 
